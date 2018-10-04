751958e7dfd87bf809549188b20506b396bd4b40
efigurgel <37422563+efigurgel@users.noreply.github.com>
2018-10-04 00:13:23 -0300

    Commit com telas de login e cadastro de usuario

--- Documents/NetBeansProjects/PlayerTheListener/src/playerthelistener/PlayerTheListener.java
+++ Documents/NetBeansProjects/PlayerTheListener/src/playerthelistener/PlayerTheListener.java
@@ -6,8 +6,7 @@ public class PlayerTheListener {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //alterado na we
+      
     }
     
 }
